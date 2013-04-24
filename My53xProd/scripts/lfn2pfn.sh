05ec69d58bcdfd935f9431f5a54ee5ab069a9e21
musella <pasquale.musella@cern.ch>
2013-04-24 22:54:04 +0300

    Fixed: unused variables

--- My53xProd/scripts/lfn2pfn.sh
+++ My53xProd/scripts/lfn2pfn.sh
@@ -5,4 +5,4 @@ file=$1 && shift
 protocol=remote-xrootd
 [[ -n $1 ]] && protocol=$1 && shift
 
-wget --no-check-certificate -O- "http://cmsweb.cern.ch/phedex/datasvc/xml/prod/lfn2pfn?node=T2_DE_DESY&protocol=remote-xrootd&lfn=/store/mc/Summer12/MinBias_TuneZ2star_8TeV-pythia6/AODSIM/NoPileUp_START50_V15-v1/0000/F6CA28C5-1670-E111-B8E7-0030487FA627.root" 2>/dev/null | sed -e "s/.*pfn='\([^']*\).*/\1\n/"
+wget --no-check-certificate -O- "http://cmsweb.cern.ch/phedex/datasvc/xml/prod/lfn2pfn?node=$site&protocol=$protocol&lfn=$dataset" 2>/dev/null | sed -e "s/.*pfn='\([^']*\).*/\1\n/"
