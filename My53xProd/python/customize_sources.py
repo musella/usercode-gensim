924a4c090e2b14d1ccc00da9f51fa9766b4749ca
Pasquale Musella <pasquale.musella@cern.ch>
2013-04-25 01:35:41 +0200

    Added PU

---
+++ My53xProd/python/customize_sources.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms
+
+from os.path import exists, join, abspath
+from os import pathsep, environ
+from string import split
+
+def search_file(filename, search_path):
+    """Given a search path, find file
+    """
+    file_found = False
+    paths = ["."]+split(search_path, pathsep)
+    for path in paths:
+        if exists(join(path, filename)):
+            file_found = True
+            break
+    if file_found:
+        return abspath(join(path, filename))
+    else:
+        return None
+    
+def replaceInputs(source):
+    fileNames = source.fileNames.value()
+    if len(fileNames) == 1 and fileNames[0].endswith('.txt'):
+        fnames = open(search_file(fileNames[0],environ['CMSSW_SEARCH_PATH']),'r')
+        source.fileNames = cms.untracked.vstring([f for f in fnames.read().split('\n') if f != '' ])
+        fnames.close()
+    
+    
+def customise(process):
+
+    if hasattr(process,'mix'):
+        replaceInputs(process.mix.input)
+        
+    replaceInputs(process.source)
+    
+    return process
