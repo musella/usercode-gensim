5b08a79dcac5ad0384eaf5204ca73ca6e7028bc5
Pasquale Musella <pasquale.musella@cern.ch>
2013-04-24 21:58:20 +0200

    dataset -> file

--- My53xProd/scripts/lfn2pfn.sh
+++ My53xProd/scripts/lfn2pfn.sh
@@ -5,4 +5,4 @@ file=$1 && shift
 protocol=remote-xrootd
 [[ -n $1 ]] && protocol=$1 && shift
 
-wget --no-check-certificate -O- "http://cmsweb.cern.ch/phedex/datasvc/xml/prod/lfn2pfn?node=$site&protocol=$protocol&lfn=$dataset" 2>/dev/null | sed -e "s/.*pfn='\([^']*\).*/\1\n/"
+wget --no-check-certificate -O- "http://cmsweb.cern.ch/phedex/datasvc/xml/prod/lfn2pfn?node=$site&protocol=$protocol&lfn=$file" 2>/dev/null | sed -e "s/.*pfn='\([^']*\).*/\1\n/"
