28c499706b60287f6edde212667c72a4e5a188bf
Pasquale Musella <pasquale.musella@cern.ch>
2013-04-26 12:48:28 +0200

    First successful production

--- My53xProd/scripts/run_cmsdriver.sh
+++ My53xProd/scripts/run_cmsdriver.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-cmsDriver.py MyGenSim/My53xProd/python/PythiaHadronizer_HToGG_M-125_cff \
-    --filein filelist:$CMSSW_BASE/src/MyGenSim/My53xProd/data/MINLO_H125.txt \
-    --filetype LHE \
-    --conditions auto:startup \
-    -s GEN,SIM,DIGI,L1,DIGI2RAW,RAW2DIGI,L1Reco,RECO \
-    --eventcontent AODSIM --datatier AODSIM \
-    --fileout GluGluToH2JetsHToGG-M-125_8TeV-powheg-minlo-pythia6.root \
-    --no_exec \
-    -n 2
+makePset() {
+    mass=$1
+    cmsDriver.py MyGenSim/My53xProd/python/PythiaHadronizer_HToGG_cff \
+	--filein MyGenSim/My53xProd/data/MINLO_H${mass}.txt \
+	--filetype LHE \
+	--conditions auto:startup \
+	-s GEN,SIM,DIGI,L1,DIGI2RAW,RAW2DIGI,L1Reco,RECO \
+	--eventcontent AODSIM --datatier AODSIM \
+	--fileout GluGluToH2JetsHToGG_M-${mass}_8TeV-powheg-minlo-pythia6.root \
+	--pileup 2012_Summer_50ns_PoissonOOTPU \
+	--pileup_input MyGenSim/My53xProd/data/minbias.txt \
+	--no_exec \
+	--customise MyGenSim/My53xProd/customize_sources \
+	--python_filename GluGluToH2JetsHToGG_M-${mass}_8TeV-powheg-minlo-pythia6.py \
+	-n 2
+}
+
+##     --dump_python \
+
+
+## for mass in $(seq 110 150 5); do
+##     makePset $mass
+## done
+
+makePset 125
